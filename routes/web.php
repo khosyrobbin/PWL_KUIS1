acb1a9981c268b987581e2a44aa648b40f992fb3
khosy robbin <khosybin@gmail.com>
2021-03-09 14:53:49 +0700

    Controller template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,14 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/about', function () {
-    return view('about');
-});
 
 
-Auth::routes();
+// Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about',[HomeController::class, 'about']);
+Route::get('/contact',[HomeController::class, 'contact']);
+Route::get('/services',[HomeController::class, 'services']);
