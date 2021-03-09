bc7a5158e9842b9c08f2e3cbbb72c52b781d90f3
khosy robbin <khosybin@gmail.com>
2021-03-09 13:59:23 +0700

    Input template

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,14 @@
+<li class="nav-item active">
+    <a class="nav-link" href="index.html">Home
+      <span class="sr-only">(current)</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="{{url('/about')}}">About Us</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="{{url('/services')}}">Our Services</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="{{url('/contact')}}">Contact Us</a>
+  </li>
