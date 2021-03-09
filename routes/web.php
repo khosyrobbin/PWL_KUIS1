322bc3b57dd1e6c41f00bd667314fa82a04b8e30
khosy robbin <khosybin@gmail.com>
2021-03-09 14:10:43 +0700

    coba

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/about', function () {
+    return view('about');
+});
 
 
 Auth::routes();
